e076328b9e7d3a06a337327e045c294cb16ee15a
james <illuillu55@hotmail.com>
2018-06-08 18:00:22 +0900

    init

---
+++ Quiz/Quiz_03.py
@@ -0,0 +1,12 @@
+# 문제3. 파이썬 경로명 s = '/usr/local/bin/python' 에서 각각의 디렉토리 경로명을 분리하여 출력하세요.
+s = '/usr/local/bin/python'
+
+print(s)
+
+s.split('/')
+
+print(s)
+
+"-".join(s)
+
+print(s)
--- test.py
+++ test.py
@@ -1,1 +1,1 @@
-print(10)se
+print(10)
